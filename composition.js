258981239724af27969fcc7f537fd529bfb50d3f
Ari Supriatna <arisupriatna@fabio-Aspire-E5-476G.localdomain>
2018-06-06 14:20:17 +0700

    modified object compisition

--- composition.js
+++ composition.js
@@ -7,6 +7,7 @@ class Cookie {
     this.name = name
     this.status = 'Mentah'
     this.ingredients = []
+    this.has_sugar = null
   }
 
   bake () {
@@ -43,10 +44,15 @@ class Ingredient {
 }
 
 class CookiFactory {
-  static create (options) {
+  static create (options, ingredient) {
     let optionList = options
+    let ingredients = ingredient
     let cookieOption = null
     let cookieOptArr = []
+    let ingredientArr = []
+    let material = []
+    let result = []
+    let materialArr = []
 
     for (let i = 0; i < optionList.length; i++) {
       if (optionList[i] === 'peanut butter') {
@@ -60,11 +66,49 @@ class CookiFactory {
       }
       cookieOptArr.push(cookieOption)
     }
-    return cookieOptArr
+    
+    for (let i = 0; i < ingredients.length; i++) {
+      var split2 = ingredients[i].split(' = ')
+      ingredientArr.push(split2)
+    }
+
+    for (let i = 0; i < ingredientArr.length; i++) {
+      for (let j = 1; j < ingredientArr[i].length; j++) {
+        var split3 = ingredientArr[i][j].split(', ')
+        material.push(split3)
+      }
+    }
+
+    for (let i = 0; i < material.length; i++) {
+      for (let j = 0; j < material[i].length; j++) {
+        var materialSplit = material[i][j].split(' : ')
+        materialArr.push(materialSplit)
+      }
+    }
+
+    for (let i = 0; i < materialArr.length; i++) {
+      var namaBahan = materialArr[i][1]
+      var jumlahBahan = materialArr[i][0]
+      result.push(new Ingredient (namaBahan, jumlahBahan))
+    }
+    
+  
+    console.log(result)
+    
+  }
+
+  static cookieRecommendation() {
+
   }
 }
 
 
 let option = fs.readFileSync('cookies.txt', 'utf-8').split('\n')
-let batch_of_cookies = CookiFactory.create(option)
-console.log(batch_of_cookies)
+let ingredient = fs.readFileSync('ingredient.txt', 'utf-8').split('\n')
+
+let batch_of_cookies = CookiFactory.create(option, ingredient)
+// console.log(batch_of_cookies)
+
+let sugarFreeFoods = CookiFactory.cookieRecommendation('Tuesday', batch_of_cookies);
+console.log
+
